b1a2f785b8b8915a3782547d2c4d176276c18938
seongyooo <seonlineseon@gmail.com>
2023-06-30 20:05:33 +0900

    Test word cloud

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/d3-cloud/1.2.5/d3.layout.cloud.min.js"></script>
+
+
 const myHeading = document.querySelector('h1');
 const myAnchor = document.querySelector('h1 a');
 
-myHeading.textContent = "Road Write";
+myHeading.textContent = "Road Right";
 myAnchor.href = "index.html";
 
 
@@ -10,52 +13,46 @@ myHeading.addEventListener('click', function() {
 });
 
 // 워드 클라우드 데이터 (임의로 생성한 예시 데이터)
+// 워드 클라우드 데이터
 const wordCloudData = [
-  { text: 'RoadRight', size: 40 },
-  { text: 'coding', size: 30 },
-  { text: 'web', size: 25 },
-  { text: 'python', size: 35 },
-  // 추가적인 단어와 크기 데이터를 워드 클라우드에 맞게 설정해줍니다.
-  // 예시 데이터는 크기(size) 값에 따라 단어의 크기가 결정됩니다.
+  "Hello", "world", "normally", "you", "want", "more", "words",
+  "than", "this"
 ];
 
 // 워드 클라우드 생성 및 표시 함수
 function createWordCloud(data) {
-  const width = 600;
-  const height = 400;
+  const width = 500;
+  const height = 500;
 
-  // 워드 클라우드 레이아웃 설정
-  const layout = d3.layout.cloud()
+  cloud()
     .size([width, height])
-    .words(data)
+    .words(data.map(function(d) {
+      return { text: d, size: 10 + Math.random() * 90 };
+    }))
     .padding(5)
-    .rotate(() => 0) // rotate 함수를 랜덤으로 지정하지 않음
-    .fontSize(d => d.size)
-    .on("end", draw);
+    .font("Impact")
+    .fontSize(function(d) { return d.size; })
+    .on("end", draw)
+    .start();
 
-  // 워드 클라우드 레이아웃 실행
-  layout.start();
-
-  // 워드 클라우드를 표시할 영역 선택
-  const wordCloudDiv = d3.select("#wordcloud");
-
-  // 워드 클라우드 그리기
   function draw(words) {
-    wordCloudDiv
+    d3.select("#wordcloud")
       .append("svg")
       .attr("width", width)
       .attr("height", height)
+      .style("border", "1px solid black")
       .append("g")
-      .attr("transform", `translate(${width / 2}, ${height / 2})`)
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
       .selectAll("text")
       .data(words)
-      .enter()
-      .append("text")
-      .style("font-size", d => d.size + "px")
-      .style("fill", "black")
+      .enter().append("text")
+      .style("font-size", function(d) { return d.size + "px"; })
+      .style("font-family", "Impact")
       .attr("text-anchor", "middle")
-      .attr("transform", d => `translate(${d.x}, ${d.y}) rotate(${d.rotate})`)
-      .text(d => d.text);
+      .attr("transform", function(d) {
+        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+      })
+      .text(function(d) { return d.text; });
   }
 }
 
