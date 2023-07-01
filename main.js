f0b5871d187db2b73430bf976518497ad5592828
seongyooo <seonlineseon@gmail.com>
2023-07-01 13:16:46 +0900

    Finally success~! load word-cloud

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/d3-cloud/1.2.5/d3.layout.cloud.min.js"></script>
-
-
 const myHeading = document.querySelector('h1');
 const myAnchor = document.querySelector('h1 a');
 
-myHeading.textContent = "Road Right";
+myHeading.textContent = "Road Write";
 myAnchor.href = "index.html";
 
 
@@ -13,48 +10,3 @@ myHeading.addEventListener('click', function() {
 });
 
 // 워드 클라우드 데이터 (임의로 생성한 예시 데이터)
-// 워드 클라우드 데이터
-const wordCloudData = [
-  "Hello", "world", "normally", "you", "want", "more", "words",
-  "than", "this"
-];
-
-// 워드 클라우드 생성 및 표시 함수
-function createWordCloud(data) {
-  const width = 500;
-  const height = 500;
-
-  cloud()
-    .size([width, height])
-    .words(data.map(function(d) {
-      return { text: d, size: 10 + Math.random() * 90 };
-    }))
-    .padding(5)
-    .font("Impact")
-    .fontSize(function(d) { return d.size; })
-    .on("end", draw)
-    .start();
-
-  function draw(words) {
-    d3.select("#wordcloud")
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .style("border", "1px solid black")
-      .append("g")
-      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
-      .selectAll("text")
-      .data(words)
-      .enter().append("text")
-      .style("font-size", function(d) { return d.size + "px"; })
-      .style("font-family", "Impact")
-      .attr("text-anchor", "middle")
-      .attr("transform", function(d) {
-        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-      })
-      .text(function(d) { return d.text; });
-  }
-}
-
-// 워드 클라우드 생성 및 표시
-createWordCloud(wordCloudData);
