543103752844c522f0aa0809e2ea46b6de834296
seongyooo <seonlineseon@gmail.com>
2023-07-08 16:44:50 +0900

    change ../index.html -> index.html

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ const myHeading = document.querySelector('h1');
 const myAnchor = document.querySelector('h1 a');
 
 myHeading.textContent = "Road Write";
-myAnchor.href = "../index.html";
+myAnchor.href = "index.html";
 
 
 myHeading.addEventListener('click', function() {
-  window.location.href = "../index.html";
+  window.location.href = "index.html";
 });
 
 // 워드 클라우드 데이터 (임의로 생성한 예시 데이터)
